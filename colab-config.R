0c0cd0edb1057a1c51eadfc4163eceab8f210b46
Ryuta2329 <77957177+Ryuta2329@users.noreply.github.com>
2022-08-06 19:02:41 -0400

    Create colab-config.R

---
+++ colab-config.R
@@ -0,0 +1,24 @@
+colab_config <- function(local_path="/usr/local/lib/python3.7/dist-packages/google/colab/_ipython.py") {
+  if (!require(googledrive))
+    install.packages(“googledrive”)
+  if (!require(httpuv))
+    install.packages("httpuv")
+
+  # Y cargo ambas librerias
+  library(googledrive) 
+  library(httpuv)
+
+  if (file.exists(local_path)) {  
+    if (!require(R.Utils))
+      install.packages("R.utils")
+
+    library("R.utils", quietly=TRUE, warn.conflicts=FALSE)
+    library("httr", quietly=TRUE, warn.conflicts=FALSE)
+
+    my_check <- function() {return(TRUE)}
+    reassignInPackage("is_interactive", pkgName = "httr", my_check)
+    options(rlang_interactive=TRUE)
+  }
+
+  drive_auth(use_oob = TRUE, cache = FALSE)
+}
