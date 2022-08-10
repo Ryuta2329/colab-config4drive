40085d1b79a2a28dbdbcf9fabc978f5aeff66e52
Ryuta2329 <77957177+Ryuta2329@users.noreply.github.com>
2022-08-10 17:39:24 -0400

    Update colab-config.R

--- colab-config.R
+++ colab-config.R
@@ -1,4 +1,4 @@
-colab_config = function(local_path="/usr/local/lib/python3.7/dist-packages/google/colab/_ipython.py") {
+colab_config <- function(local_path="/usr/local/lib/python3.7/dist-packages/google/colab/_ipython.py") {
   if (!require(googledrive, quietly = TRUE, warn.conflicts=FALSE))
     install.packages("googledrive")
   if (!require(httpuv, quietly = TRUE, warn.conflicts=FALSE))
@@ -15,7 +15,7 @@ colab_config = function(local_path="/usr/local/lib/python3.7/dist-packages/googl
     library("R.utils", quietly=TRUE, warn.conflicts=FALSE)
     library("httr", quietly=TRUE, warn.conflicts=FALSE)
 
-    my_check = function() {return(TRUE)}
+    my_check <- function() {return(TRUE)}
     reassignInPackage("is_interactive", pkgName = "httr", my_check)
     options(rlang_interactive=TRUE)
   }
